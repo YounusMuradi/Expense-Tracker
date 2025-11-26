fe8a67a3bd1bfe094bc03308f6cb491d5df26ca0
Younus Muradi <younusmuradi2023@gmail.com>
2025-11-26 16:26:05 +0430

    handle delete

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ function App() {
   const handleAdd = (transaction) => {
     setTransactions([...transactions, transaction]);
   };
+  const handleDelete= (id)=>{
+    setTransactions(transactions.filter( t=> t.id !== id))
+  }
 
   return (
     <div className="max-w-lg mx-auto mt-10">
       <AddTransaction onAdd={handleAdd} />
-      <TransactionList items={transactions} />
+      <TransactionList onDelete={handleDelete} items={transactions} />
     </div>
   );
 }
--- src/component/TransactionList.jsx
+++ src/component/TransactionList.jsx
@@ -1,28 +1,38 @@
-function TransactionList({ items }) {
+function TransactionList({ items, onDelete }) {
   if (items.length === 0)
     return <p className="mt-4 font-bold ">No transactions yet</p>;
 
-  return (
-    <div className="mt-5 space-y-3">
-      {items.map((item) => (
-        <div
-          key={item.id}
-          className={`p-3 shadow rounded flex justify-between ${
-            item.type === "income" ? "bg-green-100" : "bg-red-100"
-          }`}
-        >
-          <div>
-            <p className="font-bold">{item.title}</p>
-            <p className="text-xs text-gray-600">{item.date.toDateString()}</p>
-          </div>
+return (
+    <div className="mt-4">
+      <h2 className="mb-3 text-xl font-bold">Transactions</h2>
 
-          <p className="font-bold">
-            {item.type === "expense" ? "-" : "+"}${item.amount}
-          </p>
-        </div>
-      ))}
+      <div className="space-y-3">
+        {items.map((t) => (
+          <div
+            key={t.id}
+            className="flex items-center justify-between p-3 bg-white rounded shadow"
+          >
+            <div>
+              <p className="font-semibold">{t.title}</p>
+              <p className={t.type === "income" ? "text-green-600" : "text-red-600"}>
+                {t.type === "income" ? "+" : "-"} {t.amount}
+              </p>
+              <p className="text-xs text-gray-500">
+                {t.date.toLocaleString()}
+              </p>
+            </div>
+
+            <button
+              onClick={() => onDelete(t.id)}
+              className="font-bold text-red-600"
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default TransactionList;
